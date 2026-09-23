25fa8fb89a984053863163aa84774b2b2cdaa686
ctln-d <caitlindu10@gmail.com>
2026-09-22 21:55:06 -0400

    player and bg

--- player.gd
+++ player.gd
@@ -2,13 +2,12 @@ extends CharacterBody2D
 
 
 const SPEED = 900.0
-const JUMP_VELOCITY = -900.0
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta * 0.2
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
